319d0717c72b1bbaf39455cf27df2e94e06fad54
dix1mm <dix1mm@mail.ru>
2022-05-01 13:41:19 +0400

    Багфикс и подключение времени к UI

--- Assets/Scripts/WorkingParams/WorkingParams.cs
+++ Assets/Scripts/WorkingParams/WorkingParams.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WorkingParams : MonoBehaviour{
+    public UnityEvent<string> OnTimeChange;
     private WorkingTime _time;
 
     public IWorkingTime WorkingTime => _time;
 
     private void Awake() => _time = new WorkingTime();
 
-    private void FixedUpdate() => _time.Update(Time.fixedTime);
+    private void FixedUpdate(){
+        _time.Update(Time.fixedDeltaTime);
+        OnTimeChange.Invoke(_time.Time);
+    }
 }
